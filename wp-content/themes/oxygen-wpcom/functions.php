1e158cbd3ce4c4afeeb4188160b74653269c98fb
cami2708 <cami2708@yahoo.es>
2015-08-14 04:21:47 -0300

    Solucionando problema de incompatibilidad HTML

--- wp-content/themes/oxygen-wpcom/functions.php
+++ wp-content/themes/oxygen-wpcom/functions.php
@@ -509,3 +509,27 @@ add_filter( 'infinite_scroll_has_footer_widgets', 'oxygen_has_footer_widgets' );
 // updater for WordPress.com themes
 if ( is_admin() )
 	include dirname( __FILE__ ) . '/inc/updater.php';
+
+// Trying to attach a function
+function teacherPublication($teacherName, $teacherSurname){
+
+	$url = "https://www.googleapis.com/scribe/v1/research?query=".$teacherName."+".$teacherSurname."&dataset=paper&key=AIzaSyDqVYORLCUXxSv7zneerIgC2UYMnxvPeqQ";
+
+	$contents = file_get_contents($url); 
+	$contents = utf8_encode($contents); 
+	$results = json_decode($contents); 
+
+	$googleScholarInfoPublications = $results->data[0]->scholar;
+
+	for($i=count($googleScholarInfoPublications)-1; $i>=0; $i--){
+		echo '<div class="'.$teacherSurname.'" id="'.$googleScholarInfoPublications[$i]->title.'">';
+		echo '<a href="'.$googleScholarInfoPublications[$i]->url.'">';
+		echo '<h2 class="scholarTitle">';
+		echo $googleScholarInfoPublications[$i]->title;
+		echo '</h2><br><p class="scholarMetadata">';
+		echo $googleScholarInfoPublications[$i]->metadata;
+		echo "</p></a>";
+		echo "</div>";
+	}
+
+}
